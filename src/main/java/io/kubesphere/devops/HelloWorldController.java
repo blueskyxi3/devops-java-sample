a71eaac6e3f62c712a551fed0decede9e8657ecc
blueskyxi3 <343854933@qq.com>
2021-01-21 21:40:30 +0800

    Update HelloWorldController.java

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -4,7 +4,6 @@ package io.kubesphere.devops;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Hello world!
@@ -13,13 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @EnableAutoConfiguration
 public class HelloWorldController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String sayHello() {
         return "Really appreciate your star, that's the power of our life. v1";
-    }
-    
-    @RequestMapping("/user")
-    public String getUserInfo() {
-        return "userInfo:vincentzou";
-    }
+    } 
 }
